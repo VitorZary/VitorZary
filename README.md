521d38e8fe700a39016af79cf19a39916df4a6f5
Vitor Zary <hugozary@gmail.com>
2025-09-13 14:07:14 -0300

    Atualizando o currículo para setembro de 2025

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
 # Currículo Online com Dark Mode (Vitor Hugo Kuhn Zary)
-Meu currículo como base o currículo do falvojr com melhorias da minha parte, como comentando eu adicionei a funcionalidade do dark mode no projeto.
+Meu currículo como base o currículo do falvojr com a funcionalidade do dark mode implementada por mim.
 
 ## Link para acessar o currículo online
 https://vitorzary.github.io/VitorZary/
 
-## Segue algumas capturas de tela do projeto
+## Segue a seguir as capturas de tela do projeto
 
-### Parte 1
- ![Curriculo Online parte 1](/assets/Curriculo-pt1.png)
+ ![Currículo parte 1](/assets/curriculo1.png)
 
-### Parte 2
- ![Curriculo Online parte 2](/assets/Curriculo-pt2.png)
+ ![Currículo parte 2](/assets/curriculo2.png)
 
-### Parte 3
- ![Curriculo Online parte 3](/assets/Curriculo-pt3.png)
+ ![Currículo parte 3](/assets/curriculo3.png)
 
-### Parte 4
- ![Curriculo Online parte 4](/assets/Curriculo-pt4.png)
+ ![Curriculo parte 4](/assets/curriculo4.png)
 
+ ![Curriculo parte 5](/assets/curriculo5.png)
+
+  ![Curriculo parte 6](/assets/curriculo6.png)
--- script.js
+++ script.js
@@ -1,55 +1,45 @@
 const changeThemeBtn = document.querySelector("#change-theme");
-const labelDarkMode = document.querySelector("#labelDarkMode")
-const textDarkMode = document.querySelector("#textDarkMode")
-const leftColumn = document.querySelector("#leftColumn")
-const sobreCard = document.querySelector("#sobreCard")
-const sobreCardTitle = document.querySelector("#sobreCardTitle")
-const experienciaCard = document.querySelector("#experienciaCard")
-const experienciaCardTitle = document.querySelector("#experienciaCardTitle")
-const educacaoCard = document.querySelector("#educacaoCard")
-const educacaoCardTitle = document.querySelector("#educacaoCardTitle")
-const projetosCard = document.querySelector("#projetosCard")
-const projetosCardTitle = document.querySelector("#projetosCardTitle")
-const subtitleExperiencias = document.querySelector("#subtitleExperiencias")
-const subtitleEducacao1 = document.querySelector("#subtitleEducacao1")
-const subtitleEducacao2 = document.querySelector("#subtitleEducacao2")
+const labelDarkMode = document.querySelector("#labelDarkMode");
+const textDarkMode = document.querySelector("#textDarkMode");
+const leftColumn = document.querySelector("#leftColumn");
+const sobreCard = document.querySelector("#sobreCard");
+const sobreCardTitle = document.querySelector("#sobreCardTitle");
+const educacaoCard = document.querySelector("#educacaoCard");
+const educacaoCardTitle = document.querySelector("#educacaoCardTitle");
+const projetosCard = document.querySelector("#projetosCard");
+const projetosCardTitle = document.querySelector("#projetosCardTitle");
+const subtitleEducacao1 = document.querySelector("#subtitleEducacao1");
+const subtitleEducacao2 = document.querySelector("#subtitleEducacao2");
 
 
 changeThemeBtn.addEventListener("change", function() {
-    document.body.classList.toggle("w3-light-grey")
-    document.body.classList.toggle("dark")
+    document.body.classList.toggle("w3-light-grey");
+    document.body.classList.toggle("dark");
 
-    labelDarkMode.classList.toggle("w3-light-grey")
-    labelDarkMode.classList.toggle("dark")
-    textDarkMode.classList.toggle("w3-text-black")
+    labelDarkMode.classList.toggle("w3-light-grey");
+    labelDarkMode.classList.toggle("dark");
+    textDarkMode.classList.toggle("w3-text-black");
 
+    leftColumn.classList.toggle("w3-white");
+    leftColumn.classList.toggle("w3-grey");
+    leftColumn.classList.toggle("w3-text-grey");
+    leftColumn.classList.toggle("w3-text-black");
 
-    leftColumn.classList.toggle("w3-white")
-    leftColumn.classList.toggle("w3-grey")
-    leftColumn.classList.toggle("w3-text-grey")
-    leftColumn.classList.toggle("w3-text-black")
+    sobreCard.classList.toggle("w3-white");
+    sobreCard.classList.toggle("w3-grey");
+    sobreCardTitle.classList.toggle("w3-text-grey");
+    sobreCardTitle.classList.toggle("w3-text-black");
 
-    sobreCard.classList.toggle("w3-white")
-    sobreCard.classList.toggle("w3-grey")
-    sobreCardTitle.classList.toggle("w3-text-grey")
-    sobreCardTitle.classList.toggle("w3-text-black")
+    educacaoCard.classList.toggle("w3-white");
+    educacaoCard.classList.toggle("w3-grey");
+    educacaoCardTitle.classList.toggle("w3-text-grey");
+    educacaoCardTitle.classList.toggle("w3-text-black");
+
+    projetosCard.classList.toggle("w3-white");
+    projetosCard.classList.toggle("w3-grey");
+    projetosCardTitle.classList.toggle("w3-text-grey");
+    projetosCardTitle.classList.toggle("w3-text-black");
     
-    experienciaCard.classList.toggle("w3-white")
-    experienciaCard.classList.toggle("w3-grey")
-    experienciaCardTitle.classList.toggle("w3-text-grey")
-    experienciaCardTitle.classList.toggle("w3-text-black")
-
-    educacaoCard.classList.toggle("w3-white")
-    educacaoCard.classList.toggle("w3-grey")
-    educacaoCardTitle.classList.toggle("w3-text-grey")
-    educacaoCardTitle.classList.toggle("w3-text-black")
-
-    projetosCard.classList.toggle("w3-white")
-    projetosCard.classList.toggle("w3-grey")
-    projetosCardTitle.classList.toggle("w3-text-grey")
-    projetosCardTitle.classList.toggle("w3-text-black")
-    
-    subtitleExperiencias.classList.toggle("w3-opacity")
-    subtitleEducacao1.classList.toggle("w3-opacity")
-    subtitleEducacao2.classList.toggle("w3-opacity")
+    subtitleEducacao1.classList.toggle("w3-opacity");
+    subtitleEducacao2.classList.toggle("w3-opacity");
 });
